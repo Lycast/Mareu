979a54aba7a3a2e91c1b2a6a8d97eeacdddbcb6f
Anthony <anthonybrenon49@gmail.com>
2021-11-17 10:41:54 +0100

    Master commit 1

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -5,11 +5,19 @@ z
 Japp/src/main/java/anthony/brenon/mareu/service/DummyMeetingApiService.java,2\5\25a668cadf05a4de925fc1fce17c4c110fabfce0
 u
 Eapp/src/main/java/anthony/brenon/mareu/service/MeetingApiService.java,0\c\0c047d82e4b2b2df2310d1ce223c87ffeb766895
-v
-Fapp/src/main/java/anthony/brenon/mareu/ui_user/AddMeetingActivity.java,4\1\41980bcf8fb18903894e2bec5dbc82f3a5bf167a
-i
-9app/src/main/java/anthony/brenon/mareu/model/Meeting.java,f\3\f37c910f1400c6f53d0b70e6ef19bdba78e04168
 a
 1app/src/main/java/anthony/brenon/mareu/di/DI.java,e\1\e14498cf0a100d40f90ca63e528c20c9d4b64e4a
-m
-=app/src/main/java/anthony/brenon/mareu/callback/Listener.java,9\f\9ffbb09125a71894da9088d4b2aed77f1b929713
+v
+Fapp/src/main/java/anthony/brenon/mareu/ui_user/DatePickerFragment.java,d\8\d8d9adaec43fe8670ba54571463c2df8c02ee284
+~
+Napp/src/main/java/anthony/brenon/mareu/ui_user/MeetingRecyclerViewAdapter.java,a\e\ae754b5f8982f06365029f2d5fb91c7ef94f17c2
+Q
+!Test Results - UnitTestMareu.html,e\d\edeb40b00025226b41068f44ea98e775223b837b
+r
+Bapp/src/main/java/anthony/brenon/mareu/callback/OnDeleteClick.java,7\7\778f983913a383ecceee9d4580a187cd49789ab4
+u
+Eapp/src/main/java/anthony/brenon/mareu/ui_user/ListMareuActivity.java,f\8\f812e37cdc27e3ea0578123e675d07cea2e7c11d
+v
+Fapp/src/main/java/anthony/brenon/mareu/ui_user/TimePickerFragment.java,5\f\5f9471b86b52c0a5526f7b350bdecf8e7b2dad0e
+@
+app/build.gradle,f\4\f4a01d6a4fcb971362ec00a83903fd3902f52164
--- app/src/androidTest/java/anthony/brenon/mareu/test/InstrumentedTestMareu.java
+++ app/src/androidTest/java/anthony/brenon/mareu/test/InstrumentedTestMareu.java
@@ -1,25 +1,39 @@
 package anthony.brenon.mareu.test;
 
 
+import static androidx.test.espresso.Espresso.closeSoftKeyboard;
+import static androidx.test.espresso.Espresso.onData;
+import static androidx.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.pressImeActionButton;
+import static androidx.test.espresso.action.ViewActions.replaceText;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.hasChildCount;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.matcher.ViewMatchers.hasMinimumChildCount;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 
 import static anthony.brenon.mareu.utils.RecyclerViewItemCountAssertion.withItemCount;
 
+import android.widget.DatePicker;
+
+import androidx.test.espresso.contrib.PickerActions;
 import androidx.test.espresso.contrib.RecyclerViewActions;
+import androidx.test.espresso.matcher.RootMatchers;
 import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.rule.ActivityTestRule;
 
+import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -44,8 +58,6 @@ public class InstrumentedTestMareu {
 
     private ListMareuActivity activity;
     private MeetingApiService service;
-    int inputId;
-    String text;
 
     public void insertTextIntoInput(Integer inputId, String text) {
         onView(withId(inputId)).perform(typeText(text));
@@ -96,19 +108,43 @@ public class InstrumentedTestMareu {
         //test click on add fab and create activity is displayed
         onView(withId(R.id.list_meeting_fab_add)).perform(click());
         onView(withId(R.id.activity_add_meeting_layout)).check(matches(isDisplayed()));
+
         //test populate create meeting and create right back to list activity
-
-        onView(withId(R.id.ti_ed_topic)).perform(typeText("Test topic"));
-        //this.insertTextIntoInput(R.id.ti_ed_topic, "topic test");
-        onView(withId(R.id.activity_add_meeting_layout)).perform(click());
-
-        onView(withId(R.id.button_create_meeting)).perform(click());
+        onView(withId(R.id.ti_ed_topic)).perform(replaceText("Topic test"));
+        
         //chek return meeting list
-        onView(withId(R.id.activity_list_ma_reu)).check(matches(isDisplayed()));
-        //chek populate is right
-//
-//        /**onView(ViewMatchers.withId(R.id.)).;*/
+        onView(withId(R.id.btn_picker_date)).perform(click());
+        onView(withClassName(Matchers.equalTo(DatePicker.class.getName()))).perform(PickerActions.setDate(2022,10,12));
+        onView(withId(android.R.id.button1)).perform(click());
+
+        //input room
+        onView(withId(R.id.ti_ed_room)).perform(click());
+        onData(equalTo("Paris")).inRoot(RootMatchers.isPlatformPopup()).perform(click());
+
+        onView(withId(R.id.ti_ed_participants)).perform(typeText("emailtest1@gmail.com"), pressImeActionButton());//press enter
+
+        closeSoftKeyboard();
+
+        //put button create
+        onView(withId(R.id.button_create_meeting)).perform(click());
         //chek meeting is displayed
-        onView(withId(R.id.activity_list_ma_reu)).check((matches(hasChildCount(2))));
+        onView(withId(R.id.list_meetings_recycler_view)).check((matches(hasChildCount(2))));
+    }
+
+    @Test
+    public void filterDate_withSuccess() {
+        openActionBarOverflowOrOptionsMenu(InstrumentationRegistry.getInstrumentation().getTargetContext());
+        onView(withText("Date")).perform(click());
+        onView(withClassName(Matchers.equalTo(DatePicker.class.getName()))).perform(PickerActions.setDate(2021,12,12));
+        onView(withId(android.R.id.button1)).perform(click());
+        onView(withId(R.id.list_meetings_recycler_view)).check((withItemCount(1)));
+    }
+
+    @Test
+    public void filterRoom_withSuccess() {
+        openActionBarOverflowOrOptionsMenu(InstrumentationRegistry.getInstrumentation().getTargetContext());
+        onView(withText("Room")).perform(click());
+        onView(withText("Tokyo")).perform(click());
+        onView(withId(R.id.list_meetings_recycler_view)).check((withItemCount(1)));
     }
 }
